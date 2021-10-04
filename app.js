32005777215db3509fabe7e708d1925650944aca
Claire Yegian <claireyegian@gmail.com>
2021-10-03 21:43:32 -0600

    created a function to find subset words

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ for (let word of dictionary) {
     }
 }
 
-console.log(all6Words)
+let rootWord = all6Words[Math.floor(Math.random() * (all6Words.length+1))]
+let chars = rootWord.split("")
+let subWords = []
+
+function builtOf (word, characters) {
+    let tempWord = word
+    while (characters.includes(tempWord.charAt(0))) {
+        characters.splice(characters.indexOf(tempWord.charAt(0)),1)
+        tempWord = tempWord.substring(1)
+    }
+    if (tempWord.length == 0) {
+        subWords.push(word)
+    }
+}
+builtOf('tapas', ['t','a','p','s','a','j','o'])
+console.log(subWords)
