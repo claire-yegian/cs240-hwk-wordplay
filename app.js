39806a574eca2b2466021dbef7f1e04d50c052a9
Claire Yegian <claireyegian@gmail.com>
2021-10-03 22:30:36 -0600

    added comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//create a list of all 6 letter words
 let all6Words = []
 for (let word of dictionary) {
     if (word.length == 6) {
@@ -5,7 +6,11 @@ for (let word of dictionary) {
     }
 }
 
+//generate random root word
 let rootWord = all6Words[Math.floor(Math.random() * (all6Words.length+1))]
+
+//set up for finding subset words: find all 3-6 letter words, create empty list 
+//of subset words
 let subWords = []
 let all3to6 = []
 for (let word of dictionary) {
@@ -14,21 +19,22 @@ for (let word of dictionary) {
     }
 }
 
-function builtOf (word, characters) {
-    let tempWord = word
+//function to find if any word is a subset of another
+function builtOf (subWord, word) {
+    let characters = word.split("")
+    let tempWord = subWord
     while (characters.includes(tempWord.charAt(0))) {
         characters.splice(characters.indexOf(tempWord.charAt(0)),1)
         tempWord = tempWord.substring(1)
     }
     if (tempWord.length == 0) {
-        subWords.push(word)
+        subWords.push(subWord)
     }
 }
 
-console.log(rootWord)
+//loop to find all subset words of the root word
 for (let item of all3to6) {
     if (rootWord.split("").includes(item.charAt(0))) {
-        builtOf (item,rootWord.split(""))
+        builtOf (item,rootWord)
     }
 }
-console.log(subWords)
